04d5cf33342374d3a26c692ae759072ab88670ba
TED <ted@TEDs-MacBook-Pro.local>
2019-07-12 22:58:31 +0700

    update config

--- src/constants/ConfigApi.js
+++ src/constants/ConfigApi.js
@@ -1,10 +1,5 @@
 export const API_URL = "http://5be67ea948c1280013fc3e90.mockapi.io/api/test";
 export const API_URL_S = "http://5be67ea948c1280013fc3e90.mockapi.io/api";
-<<<<<<< HEAD
-export const API_URL_LV = "http://128.199.198.52:8000/api/v1";
-export const API_URL_TOKEN = "http://128.199.198.52:8000/api";
-export const API_URL_INFO = "http://128.199.198.52:8000";
-=======
 export const API_URL_LV ="http://128.199.198.52:8000/api/v1";
 export const API_URL_TOKEN ="http://128.199.198.52:8000/api";
 export const API_URL_INFO ="http://128.199.198.52:8000";
@@ -12,4 +7,3 @@ export const API_URL_INFO ="http://128.199.198.52:8000";
 // host thu 2 :
 
 export const API_URL_HOST_2 ="http://localhost:5000";
->>>>>>> 4e45b8360715bb00bc05621b903a02ea91324a60
